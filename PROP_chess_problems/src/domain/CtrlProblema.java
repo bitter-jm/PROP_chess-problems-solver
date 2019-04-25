e2e7484286466c43559c3ca0d6450a779f9198b3
joanpastor <joan.pastor@hotmail.com>
2019-04-25 06:14:15 +0200

    Pulido CtrlProblema

--- PROP_chess_problems/src/domain/CtrlProblema.java
+++ PROP_chess_problems/src/domain/CtrlProblema.java
@@ -14,71 +14,71 @@ public class CtrlProblema {
 	private Problema prob;
 	private CtrlDB DB = CtrlDB.getInstance();
 	
-	public boolean cargarProblema(String nombre) { //DONE
+	public boolean cargarProblema(String nombre) { 
 		this.prob = this.DB.getProblema(nombre);
 		if (this.prob == null) return false;
 		return true;
 	}
 	
-	public void guardarProblema() { //DONE
+	public void guardarProblema() { 
 		if (this.prob != null) {			
 			DB.saveProblema(this.prob);
 		}
 	}
 	
-	public boolean eliminarProblema() { //DONE
+	public boolean eliminarProblema() { 
 		if (this.prob.getVecesJugado() > 0) return false;
 		this.DB.deleteProblema(this.prob);
 		this.prob = null;
 		return true;
 	}
 	
-	public void crearProblema() { //DONE
+	public void crearProblema() {
 		this.prob = new Problema();	
 	}
 	
-	public boolean validarProblema() { //DONE
+	public boolean validarProblema() { 
 		if (this.prob == null) return false;
 		return this.prob.validarProblema();
 	}
 	
-	public void anadirFicha(Ficha f, int i, int j) { //DONE
+	public void anadirFicha(Ficha f, int i, int j) { 
 		if (this.prob != null) {
 			this.prob.anadirFicha(f, i, j);
 		}
 	}
 	
-	public void retirarFicha(int i, int j) { //DONE
+	public void retirarFicha(int i, int j) { 
 		if (this.prob != null) {
 			this.prob.retirarFicha(i, j);
 		}
 	}
 	
-	public void moverFicha(Movimiento mov) { //DONE
+	public void moverFicha(Movimiento mov) { 
 		if (this.prob != null) {			
 			this.prob.moverFicha(mov);
 		}
 	}
 	
-	public boolean setMaxMovimientos(int maxMovimientos) { //DONE
+	public boolean setMaxMovimientos(int maxMovimientos) { 
 		if (this.prob == null || maxMovimientos <= 0) return false;
 		this.prob.setMaxMovimientos(maxMovimientos);
 		return true;
 	}
 	
-	public boolean setColorAGanar(boolean color) { //DONE
+	public boolean setColorAGanar(boolean color) { 
 		if (this.prob == null) return false;
 		this.prob.setColorAGanar(color);;
 		return true;
 	}
 	
-	public boolean setNombre(String nombre) { //DONE
+	public boolean setNombre(String nombre) { 
 		if (this.prob != null && DB.existsProblema(nombre)) return false;
 		this.prob.setNombre(nombre);
 		return true;		
 	}
 	
-	public void setFEN(String fen) { //DONE
+	public void setFEN(String fen) { 
 		if (this.prob != null) {
 			this.prob.setFEN_Tablero(fen);
 		}
